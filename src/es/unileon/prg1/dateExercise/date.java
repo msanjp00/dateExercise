a8b4e801b1d4f1ba382aac4116e81d63dd94018d
msanjp00 <msanjp00@estudiantes.unileon.es>
2016-04-07 20:44:50 +0200

    segundo commit

---
+++ src/es/unileon/prg1/dateExercise/date.java
@@ -0,0 +1,41 @@
+package es.unileon.prg1.dateExercise;
+
+public class Date (int year, int day, int moth;) {
+		
+		int year;
+		int day;
+		int moth;
+		
+		
+		if(year==this.year){
+			
+			this.year.isSameYear(int year);		
+			
+		}
+	
+	public String monthName()
+	{
+		
+		switch (mothName) {
+				
+			case 0: System.out.println("January");
+			case 1: System.out.println("February");
+			case 2: System.out.println("March");
+			case 3: System.out.println("April");
+			case 4: System.out.println("May");
+			case 5: System.out.println("June");
+			case 6: System.out.println("July");
+			case 7: System.out.println("August");
+			case 8: System.out.println("September");
+			case 9: System.out.println("October");
+			case 10: System.out.println("November");
+			case 11: System.out.println("December");
+				
+	}
+	public Boolean mothRight;
+	public String mothSeason;
+	
+	
+		
+	
+}
