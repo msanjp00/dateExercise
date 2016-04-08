bde0efcd480e96bb28e53fb0835493af7d25a7df
msanjp00 <msanjp00@estudiantes.unileon.es>
2016-04-08 11:59:57 +0200

    Cambios pequenos

--- src/es/unileon/prg1/dateExercise/date.java
+++ src/es/unileon/prg1/dateExercise/date.java
@@ -1,39 +1,94 @@
 package es.unileon.prg1.dateExercise;
 
-public class Date (int year, int day, int moth;) {
+public class Date (int year, int day, int month;) {
 		
 		int year;
 		int day;
-		int moth;
+		int month;
 		
-		
+	
+		//comprobar
 		if(year==this.year){
 			
 			this.year.isSameYear(int year);		
 			
 		}
 	
-	public String monthName()
-	{
+	public String monthName(){
 		
-		switch (mothName) {
+		String monthReturn;
+		
+		switch (monthName) {
 				
-			case 0: System.out.println("January");
-			case 1: System.out.println("February");
-			case 2: System.out.println("March");
-			case 3: System.out.println("April");
-			case 4: System.out.println("May");
-			case 5: System.out.println("June");
-			case 6: System.out.println("July");
-			case 7: System.out.println("August");
-			case 8: System.out.println("September");
-			case 9: System.out.println("October");
-			case 10: System.out.println("November");
-			case 11: System.out.println("December");
+			case 1: monthReturn="January";
+				break;
+			case 2: monthReturn="February";
+				break;
+			case 3: monthReturn="March";
+				break;
+			case 4: monthReturn="April";
+				break;
+			case 5: monthReturn="May";
+				break;
+			case 6: monthReturn="June";
+				break;
+			case 7: monthReturn="July";
+				break;
+			case 8: monthReturn="August";
+				break;
+			case 9: monthReturn="September";
+				break;
+			case 10: monthReturn="October";
+				break;
+			case 11: monthReturn="November";
+				break;
+			case 12: monthReturn="December";
+				break;
 				
+		}
+		
+		return monttReturn;
+		
 	}
-	public Boolean mothRight;
-	public String mothSeason;
+	
+	public 	
+		
+		if ((this.day<0)||(this.day>31)){
+		
+			// falta
+		
+		}if((this.month<0)||(this.month>12){
+		
+			//falta
+		
+		}if(this.year<0){
+		
+			//falta
+		
+		}else{
+			
+		}
+		
+		switch (season) {
+			
+			case 1-3: System.out.println("Winter"):
+			case 4-6: System.out.println("Spring");
+			case 7-9: System.out.println("Summer");
+			case 10-12: System.out.println("Autumn/Fall");
+			
+		}
+		
+			
+			monthLeft=0;
+			for(i=this.month;i<12;i++){
+				
+				monthLeft=montLeft+1
+				
+			}
+		
+		
+		
+	
 	
 	
 		
